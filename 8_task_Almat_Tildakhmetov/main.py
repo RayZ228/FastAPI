c7943bbb31b1a904510cb38fafd9f2dcd421aa71
LINESKL <priveth06@gmail.com>
2025-06-19 23:46:11 +0500

    8 task completed

---
+++ 8_task_Almat_Tildakhmetov/main.py
@@ -0,0 +1,171 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlmodel import SQLModel, Session, create_engine, select, Field, Relationship
+from pydantic import BaseModel
+from typing import Optional
+import os
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from note_url import router as note_router
+
+
+app = FastAPI()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+engine = create_engine(DATABASE_URL, echo=True)
+SQLModel.metadata.create_all(engine)
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+SECRET_KEY = "1234567890abcdef1234567890abcdef"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+app.include_router(note_router, prefix="/notes", tags=["notes"])
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(UTC) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm = ALGORITHM0)
+    return encode_jwt
+
+
+def get_user(token: str = Depends(oauth2_scheme), session: Session = Depends(get_session)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid authentication credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    user = session.exec(select(User).where(User.username == username)).first()
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+def role(required_role: str):
+    def role_checker(user: User = Depends(get_user)):
+        if user.role != required_role:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Operation not permitted",
+            )
+        return user
+    return role_checker
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str
+    password: str
+    role: str = Field(default="user")
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+    role: str
+
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    role: Optional[str]
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+class Note(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    content: str
+    owner_id: int = Field(foreign_key="user.id")
+    owner: User = Relationship(back_populates="notes")
+
+class NoteCreate(BaseModel):
+    title: str
+    content: str
+
+class NoteResponse(BaseModel):
+    id: int
+    title: str
+    content: str
+    owner_id: int
+
+class NoteUpdate(BaseModel):
+    title: Optional[str] = None
+    content: Optional[str] = None
+
+
+@app.post("/register", response_model=UserResponse)
+def register(user: UserCreate, session: Session = Depends(get_session)):
+    existing_user = session.exec(
+        select(User).where(User.username == user.username)
+    ).first()
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username already exists"
+        )
+
+    user.password = get_password_hash(user.password)
+    db_user = User(username=user.username, password=user.password, role=user.role)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return UserResponse(id=db_user.id, username=db_user.username)
+
+
+@app.post("/login")
+def login(user: UserLogin, session: Session = Depends(get_session)):
+    db_user = session.exec(
+        select(User).where(User.username == user.username)
+    ).first()
+
+    if not db_user or db_user.password != user.password:
+        raise HTTPException(
+            status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials"
+        )
+    access_token = create_token({"sub": db_user.username})
+    return Token(access_token=access_token)
+
+@app.get("/users/me", response_model=UserResponse)
+def me_api(user: User = Depends(get_user)):
+    return UserResponse(id=user.id, username=user.username)
+
+@app.get("/admin/users", response_model=list[UserResponse], dependencies=[Depends(role("admin"))])
+def get_users(session: Session = Depends(get_session())):
+    users = session.exec(select(User).all())
+    return [UserResponse(id=user.id, username=user.username, role=user.role) for user in users]
